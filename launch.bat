406e01601e4c17d3427a3a824f5664091bc5c644
Moheax <39918032+Moheax@users.noreply.github.com>
2020-08-11 15:50:51 +0200

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,14 @@
+@echo off
+title Solyx Launcher
+cls 
+
+
+echo================================================
+
+echo                        Solyx
+echo================================================
+
+
+python main.py
+
+pause
