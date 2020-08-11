9e5cfe9aec3c5e38b2029f7f3d74b0ed96e16b6a
Moheax <39918032+Moheax@users.noreply.github.com>
2020-08-11 15:55:51 +0200

    Add files via upload

---
+++ abc/monsters/the_forest.py
@@ -0,0 +1,42 @@
+from cogs.rpgutils.calculator import get_level, max_health
+
+class MonsterName:
+	"""
+	This is what EVERY monster's class should look like.
+	Just use this as an example to fill in data for the rest of the monsters
+	Each file will have classes for the monsters in that location
+	"""
+	def __init__(self, userinfo):
+		self.location = "The Forest"
+		self.base_dmg = 35
+		self.base_def = 8
+		self.base_health = 60
+		self.base_xp_reward = 120
+		self.base_gold_reward = 80
+
+		_level = userinfo["enemylvl"]
+		if _level and _level != "None":
+			self.level = _level
+		else:
+			max_lvl = 40
+			min_lvl = 20
+			self.level = get_level(max_lvl, min_lvl, userinfo["level"])
+
+		_health = userinfo["enemyhealth"]
+		if _health and _health != "None":
+			self.health = _health
+		else:
+			self.health = max_health(self.base_health, self.level)
+
+	def special(self, userinfo):
+		"""
+		This will be the special attack, if this monster has any.
+		This will be used in apply_attack()
+		"""
+		return
+
+	def apply_attack(self, userinfo):
+		"""
+		This is where the monster will apply its attack on userinfo.
+		"""
+		return
---
+++ abc/users/human.py
@@ -0,0 +1,26 @@
+
+class Human:
+	"""
+	This will take data from the userinfo and create a class
+	This is where its stats will be calculated, based on different
+	factors like armor, items, etc.
+	Each race will also have different base stats, so like orcs will
+	have more base_defense etc. etc.
+	"""
+	def __init__(self, userinfo):
+		pass
+
+	def special(self, enemyinfo):
+		"""
+		Special abilities for races!!!
+		This will be used in apply_attack()
+		"""
+		return
+
+	def apply_attack(self, enemyinfo):
+		"""
+		This is where the user will apply its attack to enemies
+		"""
+		return
+
+	
