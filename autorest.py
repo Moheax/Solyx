28e43a65b3c4f87b4ba648ed7cc682e2f2d4ab98
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2020-08-14 01:00:20 +0200

    Add files via upload

--- autorest.py
+++ autorest.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import datetime
 from random import choice as randchoice
 from discord.ext import commands
 import asyncio
@@ -18,15 +19,24 @@ class autorest(commands.Cog):
 				idowo = restinfo["_id"]
 				if not restinfo["health"] >= 100:
 					randomhpgain = random.randint(1, 3)
+
 					if restinfo["health"] + randomhpgain > 100:
 						restinfo["health"] = 100
 					else:
 						restinfo["health"] = restinfo["health"] + randomhpgain
 					db.users.replace_one({ "_id": idowo }, restinfo, upsert=True)
+
+					now = datetime.datetime.now()
+
+					current_time = now.strftime("%H:%M:%S")
+
+					print(current_time+" | Added "+str(randomhpgain),"health to users!")
+				
 			await asyncio.sleep(60)
 
+
 def setup(bot):
-	n = autorest(bot)
+	n = autorest(bot)	
 	loop = asyncio.get_event_loop()
 	loop.create_task(n.rest_add_hp())
 	bot.add_cog(n)
