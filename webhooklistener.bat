e9083ffa35ac782302336a779960debe789bd985
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2020-08-28 20:36:17 +0200

    Add files via upload

---
+++ webhooklistener.bat
@@ -0,0 +1,14 @@
+@echo off
+title WebhookListener
+cls 
+
+
+echo================================================
+
+echo                        Solyx
+echo================================================
+
+
+python webhooklistener.py
+
+pause
