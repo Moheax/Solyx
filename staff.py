8d1b98be06097388c69df13fc3c08cc553158a6d
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2020-09-02 13:31:23 +0200

    Add files via upload

--- staff.py
+++ staff.py
@@ -12,23 +12,25 @@ class staff(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	async def on_member_ban(self, member):
-		guild = member.guild
-		userinfo = db.users.find_one({ "_id": member.id })
+	async def on_user_ban(self, user):
+		user = ctx.message.author
+		guild = user.guild
+		userinfo = db.users.find_one({ "_id": user.id })
 		if not guild.id == "559328647639728128":
 			return
 		if userinfo["role"] == "Staff" or userinfo["role"] == "Developer":
 			userinfo["role"] = "Player"
-			db.users.replace_one({ "_id": member.id }, userinfo, upsert=True)
+			db.users.replace_one({ "_id": user.id }, userinfo, upsert=True)
 
-	async def on_member_remove(self, member):
-		guild = member.guild
-		userinfo = db.users.find_one({ "_id": member.id })
+	async def on_user_remove(self, user):
+		user = ctx.message.author
+		guild = user.guild
+		userinfo = db.users.find_one({ "_id": user.id })
 		if not guild.id == "559328647639728128":
 			return
 		if userinfo["role"] == "Staff" or userinfo["role"] == "Developer":
 			userinfo["role"] = "Player"
-			db.users.replace_one({ "_id": member.id }, userinfo, upsert=True)
+			db.users.replace_one({ "_id": user.id }, userinfo, upsert=True)
 
 def setup(bot):
 	n = staff(bot)
