21657c796ffbce8fce9eb54e93f72b14df7e3af7
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-05-27 16:44:19 +0200

    Update main.py

--- solyxgiveawaybot/main.py
+++ solyxgiveawaybot/main.py
@@ -5,7 +5,7 @@ intents = discord.Intents.default()
 intents.reactions = True
 
 if __name__ == '__main__':
-    TOKEN = 'NTcwNTE4MTcxODY2MzY1OTUz.XMAWcw.X_zwzgutYO4f9j3L-m3ZC9QWe68'     # bot token old | NDk1OTI4OTE0MDQ1MzA0ODQ3.D3kRGQ.MbcVySIomiC67lydfbSPfyAwc1o
+    TOKEN = ''     # bot token old | NDk1OTI4OTE0MDQ1MzA0ODQ3.D3kRGQ.MbcVySIomiC67lydfbSPfyAwc1o
     PREFIXES = ['!', '<@570518171866365953>']      # default GLOBAL prefixes to use for Solyx. mentions and server-specific prefixes are handled by SolyxClient
     COGS = ['general', 'giveaway', 'botstats']    # Default cogs to load on start-up
 
